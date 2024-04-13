25f0e22ee3315753f56c76cb5f7e61342296b74e
Shubham Kumar <54912405+Shubhamgeoinfo@users.noreply.github.com>
2024-04-13 21:58:54 +0530

    Update Home.js

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 export const Home = () => {
-  const clientId = "902fc7d9-9b05-4260-928f-bf0e06119af3";
+  const clientId = "$ClientId";
   const redirectUri = "http://localhost:8000/home";
 
   useEffect(() => {
